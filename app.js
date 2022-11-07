d266c57965f7f99f4b5647098af538d9576773db
JayMuppidi <jaikarsai.muppidi@gmail.com>
2022-11-08 02:49:02 +0530

    Backend v1
    Added backend and html to accept input, but sql error, needs to be fixed. Need to add functionality so that sql connection is closed without user having to go to http://localhost:3000

---
+++ app.js
@@ -0,0 +1,60 @@
+var sqlite3 = require('sqlite3').verbose();
+var express = require('express');
+var http = require('http');
+var path = require("path");
+var bodyParser = require('body-parser');
+var helmet = require('helmet');
+var rateLimit = require("express-rate-limit");
+
+
+var app = express();
+var server = http.createServer(app);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+
+var db = new sqlite3.Database('./database/subscribers.db');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'./public')));
+app.use(helmet());
+app.use(limiter);
+db.run('CREATE TABLE IF NOT EXISTS emp(email TEXT, name TEXT)');
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'./public/form.html'));
+  });
+
+
+// Insert
+app.post('/add', function(req,res){
+    db.serialize(()=>{
+      db.run('INSERT INTO emp(email,name) VALUES(?,?)', [req.body.id, req.body.name], function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log("New subscriber has been added");
+      //  res.send("New subscriber has been added into the database with ID = "+req.body.id+ " and Name = "+req.body.name);
+      });
+  });
+  });
+
+//Closing connection, we need to fix, rn user needs to go to http://localhost:3000/close
+  app.get('/close', function(req,res){
+    db.close((err) => {
+      if (err) {
+        res.send('There is some error in closing the database');
+        return console.error(err.message);
+      }
+      console.log('Closing the database connection.');
+      res.send('Database connection successfully closed');
+    });
+  });
+
+
+server.listen(3000,function(){ 
+    console.log("Server listening on port: 3000");
+});
